3ae418c218a9573c2b0a602e380c973162e55951
BeepinJung <bipinlamichhane@Bipins-MacBook-Pro.local>
2025-06-23 16:27:51 +0545

    Initial commit

---
+++ 1st.py
@@ -0,0 +1,5 @@
+print(""" 
+Bipin
+November 19th 2004
+Day 1 of python learning
+""")
---
+++ 2nd.py
@@ -0,0 +1,4 @@
+myName=input("What's your name:")
+print(f"Your name is {myName}")
+myAge=input("What's your age:")
+print(f"Your age is {myAge}")
